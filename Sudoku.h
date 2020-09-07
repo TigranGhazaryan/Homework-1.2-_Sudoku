544e2ecfa820d2f973f8ac87779e7b647979c813
Tigran <tigran.ghazaryan@outlook.com>
2020-09-07 09:27:30 +0400

    Update Sudoku.h

--- Sudoku.h
+++ Sudoku.h
@@ -6,18 +6,19 @@ private:
     int row{};
     int column{};
     int** matrix{};
+    bool ValidSudoku();
+    bool IsEmpty(int& row, int& column);
+    bool ColumnCheck(int row, int column, int e);
+    bool RowCheck(int row, int column, int e);
+    bool BoxCheck(int row, int column, int e);
+    bool ValidInput(int row, int column, int e);
+   
 public:
     Sudoku(int Row, int Column);
     ~Sudoku();
     void enterDigits();
-    bool IsEmpty(int& row, int& column);
-    bool ColumnCheck(int row, int e);
-    bool RowCheck(int column, int e);
-    bool BoxCheck(int row, int column, int e);
     bool solve();
     void print();
 
 };
-
-
 
