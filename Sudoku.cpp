761bdca3b57af6fc45fb3c7859c1697719e05c2b
Tigran <tigran.ghazaryan@outlook.com>
2020-09-07 09:36:06 +0400

    Update Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -4,7 +4,7 @@
 //Constructor
 Sudoku::Sudoku(int Row, int Column) : row(Row), column(Column)
 {
-    matrix = new int* [Row];
+    matrix =  new int* [Row];
     for (int i = 0; i < Row; ++i)
         matrix[i] = new int[Column];
 }
@@ -17,27 +17,43 @@ Sudoku::~Sudoku()
     delete[] matrix;
 };
 
-//Enter Digits
+//1.Check for Valid Sudoku Pattern
+bool Sudoku::ValidSudoku()
+{
+    for (int i = 0; i < row; ++i)
+        for (int j = 0; j < column; ++j)
+            if (!ValidInput(j, i, matrix[j][i]))
+                return false;
+    return true;
+}
+
+//2.Enter Digits
 void Sudoku::enterDigits()
 {
-    for (int i = 0; i < 9; ++i)
-        for (int j = 0; j < 9; ++j)
+    for (int i = 0; i < row; ++i)
+        for (int j = 0; j < column; ++j)
             std::cin >> matrix[i][j];
+
+    if (!ValidSudoku())
+    {
+        std::cout << "Invalid Sudoku Pattern\n"
+            << "Input another one!\n";
+        enterDigits();
+    }
 }
 
 
-
-//1. Row/Column is Empty (== 0)
+//3. Row/Column is Empty 
 bool Sudoku::IsEmpty(int& row, int& column)
 {
-    for (int i = 0; i < 9; ++i)
+    for (int i = 0; i < this->row; ++i)
     {
-        for (int j = 0; j < 9; ++j)
+        for (int j = 0; j < this->column; ++j)
         {
             if (matrix[j][i] == 0)
             {
-                row = i;
-                column = j;
+                row = j;
+                column = i;
                 return true;
             }
         }
@@ -45,74 +61,80 @@ bool Sudoku::IsEmpty(int& row, int& column)
     return false;
 }
 
-//2. Column Check
-bool Sudoku::ColumnCheck(int row, int e)
+//4. Column Check
+bool Sudoku::ColumnCheck(int row, int column, int e)
 {
-    for (int i = 0; i < 9; ++i)
-        if (matrix[row][i] == e)
+    for (int i = 0; i < this->column; ++i)
+        if (e < 0 || e > 9 || (e > 0 && column != i && matrix[row][i] == e))
             return false;
     return true;
 }
 
-//3. Row Check
-bool Sudoku::RowCheck(int column, int e)
+//5. Row Check
+bool Sudoku::RowCheck(int row, int column, int e)
 {
-    for (int i = 0; i < 9; ++i)
-        if (matrix[i][column] == e)
+    for (int i = 0; i < this->row; ++i)
+        if (e < 0 || e > 9 || (e > 0 && row != i && matrix[i][column] == e))
             return false;
     return true;
 }
 
-//4. BoxCheck
+//6. BoxCheck
 bool Sudoku::BoxCheck(int row, int column, int e)
 {
     int row0 = (row / 3) * 3;
     int column0 = (column / 3) * 3;
     for (int i = 0; i < 3; ++i)
         for (int j = 0; j < 3; ++j)
-            if (matrix[row0 + i][column0 + j] == e)
+            if (e < 0 || e > 9 || (e > 0 && row != (row0 + i) && column != (column0 + j) && matrix[row0 + i][column0 + j] == e))
                 return false;
     return true;
 }
 
-//Solve Sudoku
+//7.Check for ValidInput
+bool Sudoku::ValidInput(int row, int column, int e)
+{
+    if (RowCheck(row, column, e) && ColumnCheck(row, column, e) && BoxCheck(row, column, e))
+        return true;
+    return false;
+}
+
+//8.Solve Sudoku
 bool Sudoku::solve()
 {
-    int i{};
-    int j{};
-    
+    int row{};
+    int column{};
+
     //Base case
-    if (!IsEmpty(i, j))
+    if (!IsEmpty(row, column))
         return true;
 
-    if (IsEmpty(i, j))
+    if (IsEmpty(row, column))
     {
         for (int e = 1; e <= 9; ++e)
         {
-            if (RowCheck(i, e) && ColumnCheck(j, e) && BoxCheck(j, i, e))
+            if (ValidInput(row, column, e))
             {
-                matrix[j][i] = e;
+                matrix[row][column] = e;
                 if (solve())
-                   return true;  
+                    return true;
                 //Backtrack
-                matrix[j][i] = 0;
+                matrix[row][column] = 0;
             }
         }
         return false;
     }
 }
 
-//Print Solution
+//9.Print Solution
 void Sudoku::print()
 {
     std::cout << "\n";
-    for (int i = 0; i < 9; ++i)
+    for (int i = 0; i < row; ++i)
     {
-        for (int j = 0; j < 9; ++j)
+        for (int j = 0; j < column; ++j)
             //Updated the printing order (matrix [i][j] is the correct output)
             std::cout << matrix[i][j] << " ";
         std::cout << "\n";
-    };
+    }
 }
-
-
